c8b4b691c2de93c4a274d77b3dcacee72d6db6a9
Mael <74704275+maelchvlr@users.noreply.github.com>
2025-04-18 23:52:11 +0200

    opengl shenanigans

--- OpenGL 2/OpenGLShaders/Camera.h
+++ OpenGL 2/OpenGLShaders/Camera.h
@@ -2,8 +2,8 @@
 
 #include <GL\glew.h>
 
-#include <glm.hpp>
-#include <gtc\matrix_transform.hpp>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
 
 #include <GLFW\glfw3.h>
 
--- OpenGL 2/OpenGLShaders/main.cpp
+++ OpenGL 2/OpenGLShaders/main.cpp
@@ -8,9 +8,9 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
-#include <glm.hpp>
-#include <gtc\matrix_transform.hpp>
-#include <gtc\type_ptr.hpp>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 
 #include "CommonValues.h"
 
